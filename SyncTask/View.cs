811c45bc09d98b8ddfda448b5e309fc54e72f238
Steven Hall <stevenbryanb.work@gmail.com>
2025-03-22 16:19:02 +0100

    Add: View with message methods

--- SyncTask/View.cs
+++ SyncTask/View.cs
@@ -1,12 +1,41 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SyncTask
 {
     public class View : IView
     {
 
+        /// <summary>
+        /// Method to be used when displaying messages without enter in the end
+        /// for user in console
+        /// </summary>
+        /// <param name="msg">
+        /// String with message content to be displayed to user</param>
+        public void LineMessage(string msg)
+        {
+            // display message in console
+            Console.Write(msg);
+        }
+
+
+        /// <summary>
+        /// Method to be used when displaying messages for user in console
+        /// </summary>
+        /// <param name="msg">
+        /// String with message content to be displayed to user</param>
+        public void Message(string msg)
+        {
+            // display message in console
+            Console.WriteLine(msg);
+        }
+
+
+        public void ErrorMesage(string errorMsg)
+        {
+            // display error message in console
+            Console.WriteLine(errorMsg);
+        }
     }
+
+
 }
