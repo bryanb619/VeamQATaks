efff62d0ac449b414baa4c1523bb07fc0da830f0
Steven Hall <stevenbryanb.work@gmail.com>
2025-03-22 16:21:10 +0100

    Add Controller base methods

--- SyncTask/Controller.cs
+++ SyncTask/Controller.cs
@@ -7,6 +7,29 @@ namespace SyncTask
 {
     public class Controller
     {
-        
+        /// <summary>
+        /// Reference to model class
+        /// </summary>
+        private readonly Model _model;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="model">Reference to be passed for the model class
+        /// </param>
+        public Controller(Model model)
+        {
+            _model = model;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Start(IView view)
+        {
+
+
+        }
+
     }
 }
